4741b5d72f783408b1ce39ef9245218a29a703a3
RoccoCerro <rcerro02@gmail.com>
2024-07-05 18:17:41 +0200

    Add: partial style for app apartments

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <h1>Header</h1>
-    <ul>
+    <ul class="list-unstyled">
+      <li>
+        <RouterLink class="nav-link" :to="{ name: 'home' }">Casa</RouterLink>
+      </li>
       <li>
         <RouterLink class="nav-link" :to="{ name: 'apartments' }">Appartamenti</RouterLink>
+      </li>
+      <li>
+        <a href="http://127.0.0.1:8000/login">
+          <font-awesome-icon :icon="['fas', 'user']" />
+          Accedi alla tua pagina personale
+        </a>
+      </li>
+      <li>
+        <a href="http://127.0.0.1:8000/register">Registrati</a>
       </li>
     </ul>
   </div>
