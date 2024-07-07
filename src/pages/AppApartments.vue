6de91d492236c3dd3fa700504f78040611d6e483
casalaspro <casalaspro.alessandro@gmail.com>
2024-07-07 16:49:21 +0200

    ADD - calculateLimitsLatLon() method

--- src/pages/AppApartments.vue
+++ src/pages/AppApartments.vue
@@ -12,12 +12,21 @@
   <div class="container search-bar">
     <form class="form-search-latitude my-3"action="">
       <label for="complete_address" class="form-label">Inserisci la latitudine</label>
-      <input type="text" class="form-control my-input-address" id="complete_address" name="complete_address" value="" required placeholder="Inserisci la Via e scegli tra quelle suggerite">
+      <input v-model.number="latitude" @input="calculateLimitsLatLon" type="number" class="form-control my-input-address" id="complete_address" name="complete_address" placeholder="Inserisci la Via e scegli tra quelle suggerite">
       <label for="complete_address" class="form-label">Inserisci la longitudine</label>
-      <input type="text" class="form-control my-input-address" id="complete_address" name="complete_address" value="" required placeholder="Inserisci la Via e scegli tra quelle suggerite">
+      <input v-model.number="longitude" @input="calculateLimitsLatLon" type="number" class="form-control my-input-address" id="complete_address" name="complete_address" placeholder="Inserisci la Via e scegli tra quelle suggerite">
       <label for="complete_address" class="form-label">Inserisci la distanza in Chilometri</label>
-      <input type="text" class="form-control my-input-address" id="complete_address" name="complete_address" value="" required placeholder="Inserisci la Via e scegli tra quelle suggerite">
+      <input v-model.number="distance" @input="calculateLimitsLatLon" type="number" class="form-control my-input-address" id="complete_address" name="complete_address" placeholder="Inserisci la Via e scegli tra quelle suggerite">
     </form>
+    <div class="search-bar_solutions">
+      <h4>Latitudine: </h4><span>{{ latitude }}</span>
+      <h4>Longitudine: </h4><span>{{ longitude }}</span>
+      <h4>Distanza: </h4><span>{{ distance }}</span>
+      <h4>Latitudine Minima: </h4><span>{{ bounds.latMin }}</span>
+      <h4>Latitudine Massima: </h4><span>{{ bounds.latMax }}</span>
+      <h4>Longitudine Minima: </h4><span>{{ bounds.lonMin }}</span>
+      <h4>Longitudine Massima: </h4><span>{{ bounds.lonMax }}</span>
+    </div>
 
     <hr>
   </div>
@@ -82,18 +91,23 @@ export default {
 
     },
     // this function takes latitude, longitude, distance in kilometers and calculates the bounds
-    calculateBoundingBox(lat, lon, distanceKm){
+    calculateLimitsLatLon(){
+      const lat = this.latitude;
+      const lon = this.longitude;
+      const distanceKm = this.distance;
       // i create the constances that i will use
       const kmPerDegreeLat = 110.574;
       const kmPerDegreeLon = 111.320 * Math.cos(lat * (Math.PI / 180));
 
-      // i calculate the bounds
-      const latMin = lat - (distanceKm / kmPerDegreeLat);
-      const latMax = lat + (distanceKm / kmPerDegreeLat);
-      const lonMin = lon - (distanceKm / kmPerDegreeLon);
-      const lonMax = lon + (distanceKm / kmPerDegreeLon);
+    console.log(lat);
 
-      // i pass the results to my datas
+      // // i calculate the bounds
+        const latMin = lat - (distanceKm / kmPerDegreeLat);
+        const latMax = lat + (distanceKm / kmPerDegreeLat);
+        const lonMin = lon - (distanceKm / kmPerDegreeLon);
+        const lonMax = lon + (distanceKm / kmPerDegreeLon);
+
+      // // i pass the results to my datas
       this.bounds.latMin = latMin;
       this.bounds.latMax = latMax;
       this.bounds.lonMin = lonMin;
@@ -103,7 +117,7 @@ export default {
   },
   created() {
     this.fetchApartments();
-    this.calculateBoundingBox(this.latitude, this.longitude, this.distance)
+    this.calculateLimitsLatLon(this.latitude, this.longitude, this.distance)
   },
 }
 </script>
