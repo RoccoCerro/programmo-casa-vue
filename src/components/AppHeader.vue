fdbc414305b9d2ed7feb52e625e02d671e1a61aa
RoccoCerro <rcerro02@gmail.com>
2024-07-12 12:49:09 +0200

    Add: style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,23 +1,50 @@
 <template>
-  <div>
-    <ul class="list-unstyled">
-      <li>
-        <RouterLink class="nav-link" :to="{ name: 'home' }">Casa</RouterLink>
-      </li>
-      <li>
-        <RouterLink class="nav-link" :to="{ name: 'apartments' }">Appartamenti</RouterLink>
-      </li>
-      <li>
-        <a href="http://127.0.0.1:8000/login">
-          <font-awesome-icon :icon="['fas', 'user']" />
-          Accedi alla tua pagina personale
-        </a>
-      </li>
-      <li>
-        <a href="http://127.0.0.1:8000/register">Registrati</a>
-      </li>
-    </ul>
-  </div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <img class="logo-navbar" src="/public/img/logo_programmo_casa_grande.png" alt="">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link active" :to="{ name: 'home' }">Casa</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" :to="{ name: 'apartments' }">Appartamenti</RouterLink>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="text-decoration-none text-dark dropdown-item" href="http://127.0.0.1:8000/login">
+                  <font-awesome-icon :icon="['fas', 'user']" />
+                  Accedi
+                </a>
+              </li>
+              <li>
+                <a class="text-decoration-none text-dark dropdown-item" href="http://127.0.0.1:8000/register">Registrati</a>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -27,5 +54,5 @@
 </script>
 
 <style lang="scss" scoped>
-
+@use '../style/partials/header';
 </style>
